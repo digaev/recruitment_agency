c29e16731042d8eeb987a11001f05acc8b600647
Nikolay Digaev <ffs.cmp@gmail.com>
2016-11-25 12:57:32 +0500

    Remove serializers

--- api/app/serializers/employee_serializer.rb
+++
@@ -1,4 +0,0 @@
-class EmployeeSerializer < ActiveModel::Serializer
-  attributes :id, :address, :email, :name, :phone, :salary, :status, :skills
-  attributes :created_at
-end
--- api/app/serializers/vacancy_serializer.rb
+++
@@ -1,3 +0,0 @@
-class VacancySerializer < ActiveModel::Serializer
-  attributes :id, :title, :address, :salary, :skills, :created_at, :expire_at
-end
